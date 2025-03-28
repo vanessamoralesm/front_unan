4753c069e136e96d9730eb803c5445ad9f2b7245
osmarpb <opb@wayakna.com>
2025-03-28 11:06:11 -0600

    styles: añade el hover a los links del header

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,3 +1,4 @@
+import { linksHeader } from "@/lib/utils";
 import Container from "../Container";
 import { Button } from "../ui/button";
 import { Link } from 'react-router-dom'
@@ -10,13 +11,17 @@ export default function Header() {
           <Link to={'#'}>
             Logo
           </Link>
-          <nav className="text-gray-400 text-sm flex items-center gap-4">
-              <Link to={'#'}>Inicio</Link>
-              <Link to={'#'}>Maestros</Link>
-              <Link to={'#'}>Proyectos</Link>
-              <Link to={'#'}>Eventos</Link>
-              <Link to={'#'}>Contactos</Link>
-              <Link to={'#'}>Inicio</Link>
+          <nav>
+            <ul className=" text-sm flex items-center gap-4">
+                {
+                  linksHeader.map((link, index) => (
+                    <li key={index}>
+                        <Link className="text-gray-400 transition-colors duration-300 hover:text-blue-primary" to={link.href}>{link.name}</Link>
+                    </li>
+                  ))
+                }
+              
+            </ul>
           </nav>
           <Button variant={'blue'}>Iniciar sesión</Button>
         </div>
