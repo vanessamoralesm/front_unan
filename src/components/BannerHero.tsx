e9240f99647efd5cc649751b0859f4eae0e5fee4
OsmarPB <osmarperera10@gmail.com>
2025-04-05 22:46:05 -0600

    modificacion del componente banner

---
+++ src/components/BannerHero.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import Container from "./Container";
+
+interface Props {
+  className?: string,
+  title: string,
+  description?: string,
+  url?:string,
+  color:string,
+}
+
+export default function BannerHero({ className = '', title, description = '', url, color }: Props) {
+  
+  const background = ` linear-gradient(${color},${color}) ,url(${url})`
+
+  return (
+    <div
+      className={cn("py-24 text-white", className)}
+      style={{background}}
+    >
+      <Container>
+        <div>
+          <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+          {description && <p className="text-white/80 max-w-3xl">
+            {description}
+          </p>}
+        </div>
+      </Container>
+    </div>
+  )
+}
--- src/components/landing/Banner.tsx
+++ src/components/landing/Banner.tsx
@@ -1,28 +1,44 @@
+import { Link } from "react-router-dom"
+import { Button } from "../ui/button"
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   className?: string,
-  children?: React.ReactNode,
-  url?:string,
+  url?: string,
   variant?: 'default' | 'transparent'
-}  
+  withFigures?: boolean,
+  text: string,
+  textButton: string,
+  href: string,
+}
 
-export default function Banner({ children,className = '', url, variant = 'default' }: Props) {
+export default function Banner({ className = '', url, variant = 'default', withFigures = true,text,textButton,href }: Props) {
 
   let backgroundImage = ``
   let currentURL = url ? `url(${url})` : ''
 
-  if(variant === 'default') {
+  if (variant === 'default') {
     backgroundImage = ` linear-gradient( #243982E6,#243982E6) , ${currentURL}`
   }
-  if(variant === 'transparent') {
+  if (variant === 'transparent') {
     backgroundImage = ` linear-gradient( rgba(0, 0, 0, 0.27), rgba(0, 0, 0, 0.27)) , ${currentURL}`
   }
 
   return (
-    <div className={`p-18 bg-blue-primary text-white uppercase  text-3xl overflow-hidden relative  bg-no-repeat bg-center bg-cover ${className} `} style={{background:backgroundImage}}> 
-      <div className="circle size-40 rounded-full border border-white absolute top-0 left-0 -translate-x-1/6 -translate-y-3/5"></div>
-      <div className="circle size-40 rounded-full border border-white absolute top-0 left-0 -translate-x-4/6 translate-y-1/12"></div>
-        {children}
+    <div className={`p-18 bg-blue-primary flex items-center justify-between text-white uppercase  text-3xl overflow-hidden relative  bg-no-repeat bg-center bg-cover ${className} `} style={{ background: backgroundImage }}>
+      {
+        withFigures && (
+          <>
+            <div className="circle size-40 rounded-full border border-white absolute top-0 left-0 -translate-x-1/6 -translate-y-3/5"></div>
+            <div className="circle size-40 rounded-full border border-white absolute top-0 left-0 -translate-x-4/6 translate-y-1/12"></div>
+          </>
+        )
+      }
+      <p className="max-w-xl">{text}</p>
+      <Button asChild={true} variant={'outline'} className="bg-transparent" size={'lg'}>
+        <Link to={href}>
+          {textButton}
+        </Link>
+      </Button>
     </div>
   )
 }
