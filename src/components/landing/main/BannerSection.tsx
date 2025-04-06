aac20ffcf640c4843881d11b2108428f6ddd17a8
OsmarPB <osmarperera10@gmail.com>
2025-04-05 22:46:44 -0600

    modificacion del banner

--- src/components/landing/main/BannerSection.tsx
+++ src/components/landing/main/BannerSection.tsx
@@ -1,18 +1,15 @@
 import Container from "@/components/Container";
 import Banner from "../Banner";
-import { Button } from "@/components/ui/button";
-import Hero from '@/assets/img/scroll/hero.png'
 
 export default function BannerSection() {
   return (
     <section className="py-20">
         <Container>
-            <Banner className="flex items-center justify-between" url={Hero}>
-                <span className="max-w-xl">DISFRUTA DE LOS PROXIMOS EVENTOS DE LA CARRERA </span>
-                <Button variant={'outline'} className="bg-transparent py-6 px-8">
-                    REGISTRARSE
-                </Button>
-            </Banner>
+            <Banner 
+                text="DISFRUTA DE LOS PROXIMOS EVENTOS DE LA CARRERA" 
+                textButton="REGISTRARSE"
+                href={'/eventos'}
+            />
         </Container>
     </section>
   )
